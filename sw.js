a98cf5ad82e973d4c95f7ce6038639a9611a34d4
Matheus <matheusjuliano22icm@gmail.com>
2026-08-29 08:58:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,41 @@
-self.addEventListener('install', e => { 
+const CACHE = 'nilson-pdv-offline-v1';
+const FILES = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.svg'
+];
 
-    self.skipWaiting(); 
-
-});
-
-
-
-self.addEventListener('activate', e => { 
-
-    return self.clients.claim(); 
-
-});
-
-
-
-self.addEventListener('fetch', e => {
-
-    // Um fetch básico para passar na validação do PWA
-
-    e.respondWith(
-
-        fetch(e.request).catch(() => new Response("Offline"))
-
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then(cache => cache.addAll(FILES))
+            .then(() => self.skipWaiting())
     );
+});
 
-}); 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+        ).then(() => self.clients.claim())
+    );
+});
 
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        fetch(event.request).then(response => {
+            if (response && response.ok) {
+                const copy = response.clone();
+                caches.open(CACHE).then(cache => cache.put(event.request, copy));
+            }
+            return response;
+        }).catch(() =>
+            caches.match(event.request).then(cached =>
+                cached || caches.match('./index.html')
+            )
+        )
+    );
+});
